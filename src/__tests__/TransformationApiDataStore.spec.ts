cc61fd65aaea4410eb97e43d11536779b5f38464
Sy Le <lenguyensy@gmail.com>
2020-07-12 18:36:37 -0700

    update test and examples for request and response transform

--- src/__tests__/TransformationApiDataStore.spec.ts
+++ src/__tests__/TransformationApiDataStore.spec.ts
@@ -5,25 +5,25 @@ const myTransformationApiDataStoreInstance = new TransformationApiDataStore();
 
 test('Simple Request Transformation API should work - transform before request is sent', () => {
   const apiResponse = <ApiResponse>(
-    myTransformationApiDataStoreInstance.doSimpleRequestTransformApi({a: 1, b: 2})
+    myTransformationApiDataStoreInstance.doSimpleRequestTransformApi({ a: 1, b: 2 })
   );
 
   return apiResponse.result.then((resp) => {
     expect(apiResponse.ok).toBe(true);
     expect(apiResponse.status).toEqual(200);
-    expect(resp.json).toEqual('');
+    expect(apiResponse.request_body).toEqual('{"a":100,"b":400}');
+    expect(resp.json).toEqual({ a: 100, b: 400 });
   });
 });
 
-
 test('Simple Request Transformation API should work - transform before response is returned', () => {
   const apiResponse = <ApiResponse>(
-    myTransformationApiDataStoreInstance.doSimpleResponseTransformApi({ a: 1, b: 2 })
+    myTransformationApiDataStoreInstance.doSimpleResponseTransformApi({ a: 300, b: 700 })
   );
 
   return apiResponse.result.then((resp) => {
     expect(apiResponse.ok).toBe(true);
     expect(apiResponse.status).toEqual(200);
-    expect(resp.json).toEqual('');
+    expect(resp).toEqual({ sum: 1000 });
   });
 });
--- src/__tests__/TransformationApiDataStore.ts
+++ src/__tests__/TransformationApiDataStore.ts
@@ -1,16 +1,46 @@
 import { RestClient, RestApi, RequestBody, PathParam, QueryParams } from '../index';
 
+interface NumberPair {
+  a: number;
+  b: number;
+}
+
 @RestClient({
   baseUrl: 'https://httpbin.org',
 })
 export class TransformationApiDataStore {
+  // this example will transform the request to make a and b and multiply
+  // them by 100 and 200 respectively before sending them to the back end
   @RestApi('/anything', {
     method: 'POST',
-  })
-  doSimpleRequestTransformApi(@RequestBody requestBody: object): any {}
+    request_transform: (fetchOptions: Request, pair: NumberPair): Promise<Request> => {
+      const newBody = {
+        a: pair.a * 100,
+        b: pair.b * 200,
+      };
 
+      return Promise.resolve(
+        Object.assign(fetchOptions, {
+          body: JSON.stringify(newBody),
+        }),
+      );
+    },
+  })
+  doSimpleRequestTransformApi(@RequestBody requestBody: NumberPair): any {}
+
+  // this example will make the api response to the backend
+  // then transform the returned data by adding the 2 numbers a and b
+  // and return the result as a sum
   @RestApi('/anything', {
     method: 'POST',
+    response_transform: (fetchOptions: Request, resp: Response): Promise<any> => {
+      return resp.json().then((respJson) => {
+        const pair = <NumberPair>JSON.parse(respJson.data);
+        const sum = pair.a + pair.b;
+
+        return { sum };
+      });
+    },
   })
-  doSimpleResponseTransformApi(@RequestBody requestBody: object): any {}
+  doSimpleResponseTransformApi(@RequestBody requestBody: NumberPair): any {}
 }
