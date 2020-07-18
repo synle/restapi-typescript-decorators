15219c376e2aeaf87399468628b46bb5cd49ee91
Sy Le <lenguyensy@gmail.com>
2020-07-18 09:07:49 -0700

    added mock server to do the retry

---
+++ mock-server/index.js
@@ -0,0 +1,26 @@
+var restify = require('restify');
+var errs = require('restify-errors');
+
+
+
+function between(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function respond(req, res, next) {
+  const num = between(1, 10);
+  if(num <= 4){
+    res.send({ message: 'SUCCESS' });
+    next();
+  } else {
+    return next(new Error('FAILED'));
+  }
+}
+
+var server = restify.createServer();
+server.get('/hello', respond);
+server.head('/hello', respond);
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
