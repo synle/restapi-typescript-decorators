7dbf70cc7ed4c78b24d310e772a89a4365c1baf4
Sy Le <lenguyensy@gmail.com>
2020-07-20 15:32:41 -0700

    supports for encoded form post

--- src/__tests__/PublicApiDataStore.ts
+++ src/__tests__/PublicApiDataStore.ts
@@ -26,29 +26,6 @@ export class PublicApiDataStore {
     @QueryParams _queryParams: HttpBinRequest,
   ): ApiResponse<HttpBinResponse> {}
 
-  // do simple post with request body
-  @RestApi('/post', {
-    method: 'POST',
-  })
-  doSimpleHttpBinPost(@RequestBody _body: HttpBinRequest): ApiResponse<HttpBinResponse> {}
-
-  // do simple post with formData
-  @RestApi('/anything', {
-    method: 'POST',
-  })
-  doSimpleFormDataHttpBinPost(
-    @FormDataBody('unitPrice') _unitPrice: number,
-    @FormDataBody('quantity') _qty: number,
-  ): ApiResponse<HttpBinResponse> {}
-
-  // this example uploads the file via input named `mySms`
-  @RestApi('/anything', {
-    method: 'POST',
-  })
-  doSimpleUploadFileHttpBinPost(
-    @FormDataBody('mySms') _mySmsContent: HttpBinRequest,
-  ): ApiResponse<HttpBinResponse> {}
-
   // the actual API will return in 10 seconds, but the client
   // will fail and timeout in 3 seconds
   @RestApi('/delay/10', {
@@ -71,6 +48,40 @@ export class PublicApiDataStore {
     },
   })
   doSimpleHttpGetWithXmlData(): ApiResponse<HttpBinResponse> {}
+
+  // do simple post with JSON request body
+  @RestApi('/post', {
+    method: 'POST',
+  })
+  doSimpleHttpBinPostJsonBody(@RequestBody _body: HttpBinRequest): ApiResponse<HttpBinResponse> {}
+
+  // do simple post with URL encoded form request body
+  @RestApi('/post', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  })
+  doSimpleHttpBinPostEncodedForm(
+    @RequestBody _body: HttpBinRequest,
+  ): ApiResponse<HttpBinResponse> {}
+
+  // do simple post with formData
+  @RestApi('/anything', {
+    method: 'POST',
+  })
+  doSimpleFormDataHttpBinPost(
+    @FormDataBody('unitPrice') _unitPrice: number,
+    @FormDataBody('quantity') _qty: number,
+  ): ApiResponse<HttpBinResponse> {}
+
+  // this example uploads the file via input named `mySms`
+  @RestApi('/anything', {
+    method: 'POST',
+  })
+  doSimpleUploadFileHttpBinPost(
+    @FormDataBody('mySms') _mySmsContent: HttpBinRequest,
+  ): ApiResponse<HttpBinResponse> {}
 
   // this example uploads the file as a single stream
   @RestApi('/post', {
