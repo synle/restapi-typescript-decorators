b44964aff2ec46f6530f893a857449de788ee34c
Sy Le <lenguyensy@gmail.com>
2021-09-05 07:30:44 -0700

    upgrade to node-fetch3

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,14 @@
 module.exports = {
+  preset: 'ts-jest',
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*+(spec|test).+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
+    "^.+\\.(js|jsx)$": "babel-jest"
   },
+  transformIgnorePatterns: [
+    'node_modules/node-fetch',
+  ],
   setupFilesAfterEnv: ['./jest.setup.js'],
   moduleNameMapper: {
     'restapi-typescript-decorators': '<rootDir>/src/index',
