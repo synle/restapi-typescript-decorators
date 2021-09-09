271c0b4dff3800017977cfbe1c426be6aae5a4ae
Sy Le <lenguyensy@gmail.com>
2021-09-09 09:59:32 -0700

    code format

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  tabWidth: 2,
+  printWidth: 120,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+};
+
